c529364e404c4945ac8c840fc89b287ac3e109e0
Chandani99 <er.chandni99@gmail.com>
2022-05-09 01:34:17 +0530

    modified pages.css

--- footer.js
+++ footer.js
@@ -2,72 +2,70 @@ function footer (){
     let x = 
     `
     <div id="footer">
-    
-    <div id="firstone">
-        <div id="trust">
-            <h1><span class="fa fa-star checked ogreen"></span></h1>
-             <h1>Trustpilot</h1>
+       <div>
+        <div id="firstone">
+            <div id="trust">
+                <h1><span class="fa fa-star checked ogreen"></span></h1>
+                <h1>Trustpilot</h1>
+            </div>
+        <h2>
+        
+        
+            <span class="fa fa-star checked green"></span>
+        <span class="fa fa-star checked green"></span>
+            <span class="fa fa-star checked green"></span>
+        <span class="fa fa-star checked green"></span>
+        <span class="fa fa-star checked green"></span>
+             
+        </h2>
+            <p>TrustScore 4.5</p>
+            <p>2,673 reviews</p>
+            <i class="fa-brands fa-facebook-f social"></i>
+            <i class="fa-brands fa-instagram social"></i>
+        
+            <br>
+            <img src="https://www.freepnglogos.com/uploads/app-store-logo-png/google-play-and-apple-app-store-logos-22.png" alt="">
+        <p>Learn about the lyst for iphone,ipad and Andriod.</p>
+        <p>© 2022 Lyst</p>
         </div>
-       <h2>
-       
-       
-        <span class="fa fa-star checked green"></span>
-       <span class="fa fa-star checked green"></span>
-        <span class="fa fa-star checked green"></span>
-       <span class="fa fa-star checked green"></span>
-       <span class="fa fa-star checked green"></span>
-        
-      
-       
-        
-       </h2>
-        <p>TrustScore 4.5</p>
-        <p>2,673 reviews</p>
-        <i class="fa-brands fa-facebook-f social"></i>
-        <i class="fa-brands fa-instagram social"></i>
-       
-        <br>
-        <img src="https://www.freepnglogos.com/uploads/app-store-logo-png/google-play-and-apple-app-store-logos-22.png" alt="">
-       <p>Learn about the lyst for iphone,ipad and Andriod.</p>
-       <p>© 2022 Lyst</p>
+    <div id="writtenfooter">
+        <div>
+            <h3>INTERNATIONAL</h3>
+            <p>Lyst - AU</p>
+            <p>Lyst - CA</p>
+            <p>Lyst - UK</p>
+            <p>Lyst - Osterreich</p>
+            <p>Lyst - Schwiez</p>
+            <p>Lyst - Deutschland</p>
+            <p>Lyst - Espana</p>
+            <p>Lyst - France</p>
+            <p>Lyst - Italia</p>
+            <p>Lyst - China</p>
+            <p>Lyst - Belgie</p>
+            <p>Lyst - Nederland</p>
+        </div>
+        <div>
+            <h3>HELP AND INFO</h3>
+            <p>Help Center</p>
+            <p>About us</p>
+            <p>Shiping policy</p>
+            <p>Returns policy </p>
+            <p>Refund policy</p>
+            <p>Develpoers</p>
+            <p>Careers</p>
+            <p>Contact</p>
+            <p>Terms & conditions</p>
+            <p>Privacy & cookie policy</p>
+            <p>Intellectual property</p>
+            <p>Categories</p>
+            <p>Become a partner</p>
+            <p>Lyst Insights</p>
+            <p>Lyst News</p>
+            
+        </div>
+     
     </div>
-   <div id="writtenfooter">
-    <div>
-        <h3>INTERNATIONAL</h3>
-        <p>Lyst - AU</p>
-        <p>Lyst - CA</p>
-        <p>Lyst - UK</p>
-        <p>Lyst - Osterreich</p>
-        <p>Lyst - Schwiez</p>
-        <p>Lyst - Deutschland</p>
-        <p>Lyst - Espana</p>
-        <p>Lyst - France</p>
-        <p>Lyst - Italia</p>
-        <p>Lyst - China</p>
-        <p>Lyst - Belgie</p>
-        <p>Lyst - Nederland</p>
-    </div>
-    <div>
-        <h3>HELP AND INFO</h3>
-        <p>Help Center</p>
-        <p>About us</p>
-        <p>Shiping policy</p>
-        <p>Returns policy </p>
-        <p>Refund policy</p>
-        <p>Develpoers</p>
-        <p>Careers</p>
-        <p>Contact</p>
-        <p>Terms & conditions</p>
-        <p>Privacy & cookie policy</p>
-        <p>Intellectual property</p>
-        <p>Categories</p>
-        <p>Become a partner</p>
-        <p>Lyst Insights</p>
-        <p>Lyst News</p>
-        
-    </div>
-
-   </div>
+  </div>  
 </div>`
 
 return x;

854c08d875dc6ee39413ad0153b14ff1099c41e0
Shubham Randive <57911117+ShubhamRandive1@users.noreply.github.com>
2022-05-09 16:17:34 +0530

    Update signup.js

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,31 +1,31 @@
-let f=document.getElementById("form").addEventListener("submit",myfunc)
+let f = document.getElementById("form").addEventListener("submit", myfunc)
 
-let arr=JSON.parse(localStorage.getItem("signUp"))||[];
+let arr = JSON.parse(localStorage.getItem("signUp")) || [];
 console.log(arr)
 
 
-function myfunc(){
-    event.preventDefault();
+function myfunc() {
+  event.preventDefault();
 
-  let Email=document.getElementById("name").value;
-let pass=document.getElementById("password").value;
+  let Email = document.getElementById("name").value;
+  let pass = document.getElementById("password").value;
 
-let obj={
-  Email,
-  pass,
-}
- if(Email!=""&& pass!=""){
+  let obj = {
+    Email,
+    pass,
+  }
+  if (Email != "" && pass != "") {
 
-arr.push(obj);
+    arr.push(obj);
 
 
-localStorage.setItem("signUp",JSON.stringify(arr))
-
-}
-else {
-    let p=document.createElement("p");
-   p.innerText="input Credential"
-   p.style.color="red";
-   document.getElementById("ss").append(p)
-}
+    localStorage.setItem("signUp", JSON.stringify(arr))
+    alert("Account Created Successfully");
+    window.location.href = "login.html"
+  } else {
+    let p = document.createElement("p");
+    p.innerText = "input Credential"
+    p.style.color = "red";
+    document.getElementById("ss").append(p)
+  }
 }
